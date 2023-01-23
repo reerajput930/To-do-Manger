d13f0a93bebf10e6ff6fe725f2befbab17c7acd4
Riya Rajput <78618457+reerajput930@users.noreply.github.com>
2023-01-23 22:13:32 +0530

    modification

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const tasks = require('./router-modules/tasks')
 const {connectDB} =   require('./db/connect')
 require('dotenv').config()
 
 
-app.use(express.static('./dist'))
+app.use(express.static('./public'))
 app.use(express.json());
 app.use('/api/v1/tasks',tasks)
 
