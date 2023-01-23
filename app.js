739aa87f726edcb33ceb8126c67d197aa9711870
Riya Rajput <78618457+reerajput930@users.noreply.github.com>
2023-01-23 22:22:12 +0530

    changes

--- app.js
+++ app.js
@@ -28,7 +28,9 @@ app.use('/api/v1/tasks',tasks)
 const start = async()=>{
     try {
          await connectDB(process.env.MONGO_URI)
-        
+         app.listen(port,()=>{
+            console.log(`listening .....at port ${port}`)
+        })
     } catch (error) {
         console.log(error)
     }
